a1c51ebd11afa4eb6e48110bb01ca19886dc5c7f
Kevin Joshi <kevinjoshi46b@gmail.com>
2021-04-12 22:38:57 +0530

    Added form validation
    Added some basic form validation to the registration form under the profile page.

--- js/profile.js
+++ js/profile.js
@@ -4,3 +4,70 @@ function open_register_form() {
 function close_register_form() {
     document.getElementById("registration_form_div").style.display = "none";
 }
+
+function printError(Msg) {
+    alert(Msg)
+}
+
+function validateForm() {
+    var firstname = document.regform.register_firstname.value;
+    var middlename = document.regform.register_middlename.value;
+    var lastname = document.regform.register_lastname.value;
+    var gender = document.regform.register_gender.value;
+    var dob = document.regform.register_dob.value;
+    var address = document.regform.register_address.value;
+    var city = document.regform.register_city.value;
+    var pincode = document.regform.register_pincode.value;
+    var country = document.regform.register_country.value;
+    var emailid = document.regform.register_emailid.value;
+    var password = document.regform.register_password.value;
+    var repassword = document.regform.register_repassword.value;
+
+    var error = false;
+
+    var regex = /^[a-zA-Z]+$/;
+    if (regex.test(firstname) === false) {
+        printError("Please enter a valid First name");
+        error = true;
+    }
+
+    if ((middlename != "") && (regex.test(middlename) === false)) {
+        printError("Please enter a valid Middle name");
+        error = true;
+    }
+    if (regex.test(lastname) === false) {
+        printError("Please enter a valid Last name");
+        error = true;
+    }
+
+    regex = /^\S+@\S+\.\S+$/;
+    if (regex.test(emailid) === false) {
+        printError("Please enter a valid email address");
+        error = true;
+    }
+
+    if (password != repassword) {
+        printError("The passwords entered do not match!");
+        error = true;
+    }
+
+    if (middlename == "") { middlename = "-"; }
+    if (gender == "") { gender = "-"; }
+
+    if (error == true) {
+        return false;
+    } else {
+        var dataPreview = "You've entered the following details: \n" +
+            "First Name: " + firstname + "\n" +
+            "Middle Name: " + middlename + "\n" +
+            "Last Name: " + lastname + "\n" +
+            "Gender: " + gender + "\n" +
+            "Date of Birth: " + dob + "\n" +
+            "Address: " + address + "\n" +
+            "City: " + city + "\n" +
+            "Pincode: " + pincode + "\n" +
+            "Country: " + country + "\n" +
+            "Email ID: " + emailid + "\n"
+        alert(dataPreview);
+    }
+};
