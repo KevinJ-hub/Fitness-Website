a2a8b49b08f6d410527948d20c7733dfef29bab0
Kevin Joshi <kevinjoshi46b@gmail.com>
2021-04-25 14:07:20 +0530

    Combined the mobile and desktop navbar
    - Cleaned the navbar code to have only one nav tag that works for both mobile as well as desktop

--- js/common.js
+++ js/common.js
@@ -1,10 +1,23 @@
 function openNavbar() {
-    document.getElementById("hamburger-menu").style.display = "none";
-    document.getElementById("close-menu").style.display = "block";
-    document.getElementById("dropdown").style.display = "flex";
+    document.getElementsByClassName("hamburger-menu")[0].style.display = "none";
+    document.getElementsByClassName("close-menu")[0].style.display = "block";
+    document.getElementsByClassName("dropdown-div")[0].style.display = "flex";
 }
 function closeNavbar() {
-    document.getElementById("hamburger-menu").style.display = "block";
-    document.getElementById("close-menu").style.display = "none";
-    document.getElementById("dropdown").style.display = "none";
+    document.getElementsByClassName("hamburger-menu")[0].style.display = "block";
+    document.getElementsByClassName("close-menu")[0].style.display = "none";
+    document.getElementsByClassName("dropdown-div")[0].style.display = "none";
+}
+
+window.addEventListener('resize', resized);
+
+function resized() {
+    var w = window.outerWidth;
+    if (w > 600) {
+        document.getElementsByClassName("hamburger-menu")[0].style.display = "none";
+        document.getElementsByClassName("close-menu")[0].style.display = "none";
+        document.getElementsByClassName("dropdown-div")[0].style.display = "none";
+    } else {
+        document.getElementsByClassName("hamburger-menu")[0].style.display = "block";
+    }
 }
